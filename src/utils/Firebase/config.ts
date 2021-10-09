13de43558e4788984a4af6ed1ed26ae0d7ada289
Hisakazu KATO <hiszuk@gmail.com>
2021-10-09 11:49:02 +0900

    firebaseパラメータを自分用に変更

--- src/utils/Firebase/config.ts
+++ src/utils/Firebase/config.ts
@@ -2,12 +2,12 @@ import firebase from "firebase";
 
 // Firebase コンソールの「プロジェクトの設定」>「SDK の設定と構成」から「構成」を選択し、そのままコピペ
 const firebaseConfig = {
-  apiKey: "AIzaSyCm8ZVPFvB4O5YVyNqA-16zWrRpbxd0RVQ",
-  authDomain: "react-bootcamp-78947.firebaseapp.com",
-  projectId: "react-bootcamp-78947",
-  storageBucket: "react-bootcamp-78947.appspot.com",
-  messagingSenderId: "236750478038",
-  appId: "1:236750478038:web:fc2e6a6e2f856cae1c4777",
+  apiKey: "AIzaSyBVyB0wZgepbPG_j2D0vj5yPSzgKdsWiAQ",
+  authDomain: "react-bootcamp-351bc.firebaseapp.com",
+  projectId: "react-bootcamp-351bc",
+  storageBucket: "react-bootcamp-351bc.appspot.com",
+  messagingSenderId: "740212487180",
+  appId: "1:740212487180:web:06cd48ce76275cfbaaaa29"
 };
 
 // firebaseパッケージをAPI Keyで初期化
